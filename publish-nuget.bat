2b2a2c21b43061927c5a6fafcf72b3ad6a70b85a
franck gaspoz <franck.gaspoz@gmail.com>
2023-01-03 13:45:39 +0100

    add publish script

---
+++ publish-nuget.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+echo --------------------------------------------------------------------------------------
+echo publish-nuget
+echo --------------------------------------------------------------------------------------
+echo tag the version and publish the corresponding nuget from version to github and nuget
+echo parameter: version tag
+echo example: publish-nuget 1.0.15
+echo nugetkey=%nugetkey%
+echo githubkey=%githubkey%
+echo version=%1%
+echo --------------------------------------------------------------------------------------
+
+git tag -a "v%1%" -m "v%1%"
+git push origin v%1%
+rem nuget push CommandLine.NetCore/bin/Release/CommandLine.NetCore.%1%.nupkg %nugetkey% -Source https://api.nuget.org/v3/index.json -Verbosity detailed
+rem dotnet nuget push "CommandLine.NetCore/bin/Release/CommandLine.NetCore.%1%.nupkg" --api-key %githubkey% --source "github"
