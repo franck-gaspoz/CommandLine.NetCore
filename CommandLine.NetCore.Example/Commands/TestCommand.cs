890defd111cc48b93cb016ed80f1b6f27b9c1462
franck gaspoz <franck.gaspoz@gmail.com>
2023-04-30 21:53:24 +0200

    implementing support for operation method typed prototype

--- CommandLine.NetCore.Example/Commands/TestCommand.cs
+++ CommandLine.NetCore.Example/Commands/TestCommand.cs
@@ -9,18 +9,37 @@ class TestCommand : Command
     public TestCommand(Dependencies dependencies) : base(dependencies) { }
 
     protected override CommandResult Execute(ArgSet args) =>
-        For()
+        For(
+            Param("com"),
+            Opt<List<string>>("strList"),
+            Opt("flag", true),
+            Opt("option", false, 2))
+
         .Do(() => TestCommandBody)
 
         .Options(
-            Opt<List<string>>("a"),
-            Opt("b"),
-            Opt("c"),
-            Opt("d"),
-            Opt("e")
+            Opt("help", true)
             )
 
         .With(args);
+
+    /*
+        is specyfing this command syntax:
+        
+        testCommand com {string},{string} --flag? [--option str1 str2] --help? 
+        
+        example:
+        testCommand com abc,def --flag --option 1 2 --help
+        
+        run method arguments:
+        
+        TestCommandBody( 
+            string com, 
+            List<string> strList,
+            bool flag,
+            string?[] option,
+            bool help) { ...
+     */
 
     void TestCommandBody()
     {
